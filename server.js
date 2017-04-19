01e65382de83183a9df4deb0b1025ad3f0ed44ea
Yustynn Panicker <yustynn@gmail.com>
2017-04-20 00:56:03 +0800

    Try againnnnn

--- server.js
+++ server.js
@@ -7,15 +7,19 @@ app.get('/', function (req, res) {
 });
 
 
-app.post('/fan', (req, res) => {
-  temp = req.ip.split(':')
-  app.fan_ip = temp[temp.length-1]
-  console.log(req.ip)
-  res.send('kthxbye')
+app.post('/', (req, res) => {
+  temp = req.ip.split(':');
+  app.fan_ip = temp[temp.length-1];
+  console.log(req.ip);
+  res.send('kthxbye');
 })
 
-app.get('/fan', (req, res) => {
-  res.send(app.fan_ip)
+app.get('/', (req, res) => {
+  if (app.fan_ip) {
+    res.send(app.fan_ip)
+  }
+
+  res.end();
 })
 
 const server = app.listen(port, function () {

cf82d11f6661f5ffabda450927b50da8b4a067a3
Yustynn Panicker <yustynn@gmail.com>
2017-04-20 00:47:13 +0800

    Try again

--- app.js
+++ app.js
@@ -15,6 +15,5 @@ app.get('/fan', (req, res) => {
 })
 
 app.listen(app.get('port'), () => {
-  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env')); 
-  console.log('  Press CTRL-C to stop\n');
+  console.log( chalk.green(`App running on ${app.get('port')}`) )
 });
